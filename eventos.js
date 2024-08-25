34b69f2b4a007b35380c26db04cca1ad83b3fe82
Mel3343 <meligonzalezgomez12@gmail.com>
2024-08-25 05:18:39 -0300

    Solucion Paso 5

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 document.querySelector('div').addEventListener('click', function(click) {
     alert('Hola! Soy el div ☆*:.｡.o(≧▽≦)o.｡.:*☆');
-})
+
+});
+
+document.querySelector('button').addEventListener('click', function(event) {
+    event.stopPropagation();
+    alert('Hola! (´｡• ᵕ •｡`)');
+});
 });
