e63910afeb2f62b918c028a2d976ae291738a1b6
Mel3343 <meligonzalezgomez12@gmail.com>
2024-08-25 04:54:54 -0300

    eventos.js

---
+++ eventos.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.querySelector('div').addEventListener('click', function(click) {
+    alert('Hola! Soy el div ☆*:.｡.o(≧▽≦)o.｡.:*☆');
+})
+});
